58846e891f8701f862c08c4526fe1be9a6c323d6
Djontleman <jhutchinson531@gmail.com>
2022-02-14 12:49:08 +0000

    refactor: add try-catch to bootstrap function

--- server/typescript/src/index.ts
+++ server/typescript/src/index.ts
@@ -5,24 +5,32 @@ import { ApolloServer } from 'apollo-server';
 
 import { resolvers } from './resolvers/index';
 
+const MONGO_DB_URL = 'mongodb://localhost/spaceshipBookingDB';
+
 // || ========== Bootstrap Server Function ========== ||
 
-async function bootstrap(resolvers) {
-  // connect to MongoDb database
-  await connect('mongodb://localhost/spaceshipBookingDB')
-    .catch(err => console.log(err));
+async function bootstrap() {
+  try {
+    // connect to MongoDb database
+    await connect(MONGO_DB_URL);
+    console.log("Connected to Database");
+    
+    // build schema using resolvers
+    const schema = await buildSchema({ resolvers });
+    
+    // build Apollo server
+    const server = new ApolloServer({ schema });
+    
+    // listen on port
+    const { url } = await server.listen();
+    console.log(`🚀 Server ready at ${url}`);
 
-  // build schema using resolvers
-  const schema = await buildSchema({ resolvers });
-
-  // build Apollo server
-  const server = new ApolloServer({ schema });
-
-  // listen on port
-  const { url } = await server.listen();
-  console.log(`🚀 Server ready at ${url}`);  
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
 
 // || ========== Start Server ========== ||
 
-bootstrap(resolvers);
+bootstrap();
