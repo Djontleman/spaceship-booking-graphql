a6a66300eb057a9b1fbf44a7fb89155c609b48a3
Jonathan Hutchinson <jhutchinson531@gmail.com>
2023-02-17 23:12:46 +0000

    Run graphql server in server-gql

---
+++ apps/server-gql/src/index.ts
@@ -0,0 +1,32 @@
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { ApolloServer } from 'apollo-server';
+import path from "path";
+import { PrismaClient } from "@prisma/client";
+
+import { resolvers } from "../prisma/generated/type-graphql";
+
+interface Context {
+  prisma: PrismaClient;
+}
+
+async function main() {
+  const schema = await buildSchema({
+    resolvers,
+    emitSchemaFile: path.resolve(__dirname, "./generated-schema.graphql"),
+    validate: false,
+  });
+
+  const prisma = new PrismaClient();
+  await prisma.$connect();
+
+  const server = new ApolloServer({
+    schema,
+    context: (): Context => ({ prisma })
+  });
+
+  const { url } = await server.listen(4000);
+  console.log(`Server running on ${url}`)
+}
+
+main().catch(console.error)
